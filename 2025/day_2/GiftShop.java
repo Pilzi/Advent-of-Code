cec30d15f2dc07404ead2146307efffee49650d5
Pilzi <105017539+Pilzi@users.noreply.github.com>
2025-12-07 17:25:01 +0100

    2025-day-2: second part solution

--- 2025/day_2/GiftShop.java
+++ 2025/day_2/GiftShop.java
@@ -3,10 +3,40 @@ package day_2;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GiftShop {
 
     public long getSolution1() throws IOException {
+        long finalCode = 0L;
+
+        try (BufferedReader br = new BufferedReader(new FileReader("2025/day_2/input.txt"))) {
+            String line = br.readLine();
+            while (line != null) {
+                String[] codes = line.split(",");
+
+                for (String code : codes) {
+                    String[] codeIds = code.split("-");
+
+                    long firstId = Long.parseLong(codeIds[0]);
+                    long lastId = Long.parseLong(codeIds[1]);
+
+                    // Loop until the first id hits the second id
+                    while (firstId <= lastId) {
+                        if (isInvalidPattern(String.valueOf(firstId))) {
+                            finalCode += firstId;
+                        }
+                        firstId++;
+                    }
+                }
+                line = br.readLine();
+            }
+        }
+        return finalCode;
+    }
+
+    public long getSolution2() throws IOException {
 
         long finalCode = 0L;
 
@@ -25,9 +55,14 @@ public class GiftShop {
                     // Loop until the first id hits the second id
                     while (firstId <= lastId) {
 
-                        if (isInvalidPattern(String.valueOf(firstId))) {
-                            finalCode += firstId;
+                        String firstIdAsString = String.valueOf(firstId);
+                        for (int i = 1; i <= firstIdAsString.length() / 2; i++) {
+                            if (isInvalidPattern2(firstIdAsString, i)) {
+                                finalCode += firstId;
+                                i = (firstIdAsString.length() / 2) + 1;
+                            }
                         }
+
                         firstId++;
                     }
                 }
@@ -48,5 +83,27 @@ public class GiftShop {
         String secondPattern = pattern.substring(pattern.length() / 2);
 
         return firstPattern.equals(secondPattern);
+    }
+
+
+    public boolean isInvalidPattern2(String pattern, int offset) {
+
+        List<String> sequenceParts = new ArrayList<>();
+        int startIndex = 0;
+        int endIndex = offset;
+        while (endIndex <= pattern.length()) {
+            sequenceParts.add(pattern.substring(startIndex, endIndex));
+            startIndex = endIndex;
+            endIndex = endIndex + offset;
+        }
+        String possiblePattern = sequenceParts.getFirst();
+        for (int i = 1; i < sequenceParts.size(); i++) {
+            // The pattern is invalid if sequences are not matching or not everything is included inside the sequence parts array
+            if (!possiblePattern.equals(sequenceParts.get(i)) || startIndex != pattern.length()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
